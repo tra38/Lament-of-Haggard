878ebff0f201910e381989163d6c11028993fded
tra38 <tra38@users.noreply.github.com>
2015-03-30 11:45:41 -0500

    Update civgen.rb

--- civgen.rb
+++ civgen.rb
@@ -30,21 +30,23 @@ def historyX
     invention = false
     war = false
     art = false
+    enemy = nil
     loop do
         year += 5
         print "#{year}: "
         case event
         when "stop"
-            puts "Okay, let's stop now."
+            puts "Thus ends the story of our civilization, leaving behind nothing but these records."
             break
         when "library"
             if library == false
-                puts "The Grand Library was built to honor the ancestors."
+                puts "The Grand Library was built to honor our ancestors. It was a joyous occassion."
                 library = true
                 event = newevent #necessary to prevent the same event re-triggering again
                 puts event
             else
-            print "The library is practicing dark arts to honor the ancestors. Those dark arts might doom our society. "
+            puts "The Grand Library is researching a new philosophy based on the teaching of our ancestors."
+            puts "But some people are suspicious. "
                 if rand() > 0.5
                     puts "The dark arts have succeeded! We are doomed!"
                     event = "stop"
@@ -68,30 +70,31 @@ def historyX
                     puts event
                 end
             elsif library == false
-                puts "We want to buld an invention, but we need the help of our ancestors. Let's honor them!"
+                puts "We wanted to buld an invention, but we need the help of our ancestors. Let's honor them!"
                 event = "library"
             else
-                puts "The ancestors are pleased at our library and gave us an invention."
+                puts "The ancestors are pleased at our library and revealed to us an invention."
                 invention = true
                 event = newevent
                 puts event
             end
         when "war"
             if war == false
-                puts "A rival nation insulted our ancestors. TO WAR!"
+                enemy ||= plural(racegen)
+                puts "We encountered the #{enemy}. They were "+["genocidial madmen","murderous barbarians","uncivilized brutes","incredibly wealthy"].sample+" and so we had to fight against them." 
                 war = true
                 event = newevent
                 puts event
             else
-                puts "The enemy is at our gates and want to burn our city down! "
+                puts "The #{enemy} sent their armed forces at our very capital."
                     if invention == true
                         puts "Our invention ate the enemy, saving us! But now it has a taste of human flesh..."
                         event = "invention"
                     elsif rand() > 0.5
-                        puts "The enemy defats our forces and burned the city down!"
+                        puts "The hated enemy defeated our forces and burned our capital down!"
                         event = "stop"
                     else
-                        puts "We sued for peace, paying a heavy tribute. The enemy leaves us alone. The war ends."
+                        puts "We sued for peace, paying a heavy tribute. The #{enemy} leaves us alone. The war ends."
                         war = false
                         event = newevent
                         puts event
